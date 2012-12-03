9a7dc3b8a075d87c8fd2d8d28ba050e06e20634f
Patrice Paquette <paquette.patrice@gmail.com>
2012-12-03 04:29:36 -0500

    Added entities to map 1, added collision detection for firebolts

---
+++ JadeMonkey/GridBasedCollisionComponent.cpp
@@ -0,0 +1,67 @@
+#include "GridBasedCollisionComponent.h"
+#include "GameEntity.h"
+#include "VectorUtil.h"
+#include "Enums.h"
+
+vector<GridBasedCollisionComponent*> GridBasedCollisionComponent::_colliders;
+
+GridBasedCollisionComponent::GridBasedCollisionComponent(Game* game, GameEntity* entity, float radius, CollisionGroup group, CollisionConsequence consequence)
+	: CollisionComponent(game, entity)
+{
+	this->_radius = radius;
+	this->_group = group;
+	this->_colliders.push_back(this);
+	this->_consequence = consequence;
+}
+
+CollisionGroup GridBasedCollisionComponent::GetGroup(void)
+{
+	return this->_group;
+}
+
+float GridBasedCollisionComponent::GetRadius(void)
+{
+	return this->_radius;
+}
+
+void GridBasedCollisionComponent::Update(GameEntity* entity, long time)
+{
+	this->_collision = false;
+
+	for(int i = 0; i < this->_colliders.size(); i++)
+	{
+		if(this->_colliders[i]->GetGroup() != this->_group)
+		{
+			GameEntity* otherEntity = this->_colliders[i]->getEntity();
+			D3DXVECTOR3 positionOther = this->_colliders[i]->getEntity()->getPosition();
+			D3DXVECTOR3 positionThis = this->_entity->getPosition();
+			float distance = VectorUtil::Distance3f(D3DXVECTOR3(positionOther.x, 0, positionOther.z), D3DXVECTOR3(positionThis.x, 0, positionThis.z));
+
+			if(distance < this->_radius + this->_colliders[i]->GetRadius())
+			{
+				this->_collision = true;
+				this->_lastCollision = this->_colliders[i];
+
+				switch(this->_consequence)
+				{
+				case MinusSize:
+					otherEntity->setSize(otherEntity->getSize() - D3DXVECTOR3(0, 5, 0));
+					break;
+				case MinusLife:
+					otherEntity->reduceLife(25);
+					break;
+				case Kill:
+					otherEntity->SetStatus(false);
+				case Nothing:
+					break;
+				}
+			}
+		}
+	}
+}
+
+bool GridBasedCollisionComponent::Collided(void)
+{
+	return this->_collision;
+}
+
---
+++ JadeMonkey/GridBasedCollisionComponent.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "CollisionComponent.h"
+#include <vector>
+#include "Enums.h"
+
+using namespace std;
+
+class GridBasedCollisionComponent : public CollisionComponent
+{
+public:
+	GridBasedCollisionComponent(Game* game, GameEntity* entity, float radius, CollisionGroup group, CollisionConsequence consequence);
+	void Initialize(void){}
+	void Update(GameEntity* entity, long time);
+	CollisionGroup GetGroup(void);
+	float GetRadius(void);
+	bool Collided(void);
+	string GetName(void){ return "GridBasedCollisionComponent";}
+
+private:
+	float _radius;
+	CollisionGroup _group;
+	static vector<GridBasedCollisionComponent*> _colliders;
+	CollisionComponent* _lastCollision;
+	bool _collision;
+	CollisionConsequence _consequence;
+};
